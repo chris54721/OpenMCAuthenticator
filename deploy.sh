efd4d082ce1fa49f321480104a11906b448e8cfc
Chris54721 <chris54721@outlook.com>
2015-07-28 23:04:11 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_JDK_VERSION" = "openjdk6" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_JDK_VERSION" = "openjdk6" ]; then
     echo "mavenUser=$MAVEN_USER\nmavenPassword=$MAVEN_PASSWORD" >> gradle.properties
     gradle uploadArchives
 fi
