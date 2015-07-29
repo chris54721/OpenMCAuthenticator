3d79b304d912241f59590a5755f0c6d9533dde0d
Chris54721 <chris54721@outlook.com>
2015-07-29 12:13:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -e
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_JDK_VERSION" = "openjdk6" ]; then
     echo "mavenUser=$MAVEN_USER\nmavenPassword=$MAVEN_PASSWORD" >> gradle.properties
-    gradle uploadArchives
+    gradle publish
 fi
