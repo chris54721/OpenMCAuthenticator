482f6fb1004b87da4852087b1075f838ba2a8bc4
Chris54721 <chris54721@outlook.com>
2015-07-29 15:44:58 +0200

    Update build configuration

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_JDK_VERSION" = "openjdk6" ]; then
-    mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=org.apache.maven.wagon:wagon-ssh:2.9
     echo "mavenUser=$MAVEN_USER\nmavenPassword=$MAVEN_PASSWORD" >> gradle.properties
     gradle publish
 fi
