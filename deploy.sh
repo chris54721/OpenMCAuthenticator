54bd15f8f6a229b4e70d7aa44cf37f45593f6090
Chris54721 <chris54721@outlook.com>
2015-09-21 17:33:19 +0200

    Update build configuration

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
+gradle build
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_JDK_VERSION" = "openjdk6" ]; then
     echo "mavenUser=$MAVEN_USER\nmavenPassword=$MAVEN_PASSWORD" >> gradle.properties
     gradle publish
