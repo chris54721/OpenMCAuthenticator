dbbf42908ef47df50f5f84174b611451d9bce33b
Chris54721 <chris54721@outlook.com>
2017-03-15 16:01:52 +0100

    Remove unneeded javadoc

--- src/main/java/net/chris54721/openmcauthenticator/responses/RequestResponse.java
+++ src/main/java/net/chris54721/openmcauthenticator/responses/RequestResponse.java
@@ -1,34 +1,29 @@
 package net.chris54721.openmcauthenticator.responses;
 
-import net.chris54721.openmcauthenticator.OpenMCAuthenticator;
-
 import java.util.Map;
 
 /**
- * Response generated after every request made with private method sendJsonPostRequest.
- * Intended for internal use only.
- *
- * @see OpenMCAuthenticator#sendJsonPostRequest(java.net.URL, String)
+ * Base class for API responses.
  */
 public class RequestResponse {
 
-    private int responseCode = -1;
-    private Map<String, Object> data;
+  private int responseCode = -1;
+  private Map<String, Object> data;
 
-    public RequestResponse(int responseCode, Map<String, Object> data) {
-        this.responseCode = responseCode;
-        this.data = data;
-    }
+  public RequestResponse(int responseCode, Map<String, Object> data) {
+    this.responseCode = responseCode;
+    this.data = data;
+  }
 
-    public int getResponseCode() {
-        return responseCode;
-    }
+  public int getResponseCode() {
+    return responseCode;
+  }
 
-    public boolean isSuccessful() {
-        return responseCode == 200 || responseCode == 204;
-    }
+  public boolean isSuccessful() {
+    return responseCode == 200 || responseCode == 204;
+  }
 
-    public Map<String, Object> getData() {
-        return data;
-    }
+  public Map<String, Object> getData() {
+    return data;
+  }
 }
